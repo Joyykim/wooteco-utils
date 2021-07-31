fadf903c56b0dd02b5bf62e0e3cd994a887c931b
unluckyjung <ybook2012@gmail.com>
2021-08-01 00:41:04 +0900

    test: 두개의 입출력 테스트 구현

--- src/test/java/com/woowahan/techcourse/utils/WoowaScannerTest.java
+++ src/test/java/com/woowahan/techcourse/utils/WoowaScannerTest.java
@@ -1,5 +1,13 @@
 package com.woowahan.techcourse.utils;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.time.Duration;
 import org.assertj.core.util.Strings;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -7,21 +15,27 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.function.Executable;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
-
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.OutputStream;
-import java.io.PrintStream;
-import java.time.Duration;
-import java.util.Arrays;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
 
 class WoowaScannerTest {
 
     private static final Duration SIMPLE_TEST_TIMEOUT = Duration.ofSeconds(1L);
+    private PrintStream standardOut;
+    private OutputStream captor;
+
+    @BeforeEach
+    void setUp() {
+        standardOut = System.out;
+        captor = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(captor));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.setOut(standardOut);
+        System.out.println(captor.toString());
+    }
 
     private void subject(final String... args) {
         command(args);
@@ -33,38 +47,33 @@ class WoowaScannerTest {
         System.setIn(new ByteArrayInputStream(buf));
     }
 
+
     private void assertSimpleTest(final Executable executable) {
         assertTimeoutPreemptively(SIMPLE_TEST_TIMEOUT, executable);
     }
 
-    @DisplayName("기능 테스트")
+    @DisplayName("SCANNER 기능 테스트")
     @Nested
-    class FunctionTest {
+    class ScannerInputTest {
 
-        private PrintStream standardOut;
-        private OutputStream captor;
-
-        @BeforeEach
-        void setUp() {
-            standardOut = System.out;
-            captor = new ByteArrayOutputStream();
-            System.setOut(new PrintStream(captor));
-        }
-
-        @AfterEach
-        void tearDown() {
-            System.setOut(standardOut);
-            System.out.println(captor.toString());
-        }
-
-        private List<String> getOutputLines() {
-            return Arrays.asList(captor.toString().trim().split("\n"));
-        }
-
+        @DisplayName("한개의 입력을 받으면, 한개의 입력을 출력한다.")
         @ParameterizedTest
-        @ValueSource(strings = {"1", "2", "3", "4"})
-        void 전체기능_테스트1(String i) {
-            assertSimpleTest(() -> subject(i, i, i));
+        @ValueSource(strings = {"fortune", "seed", "joy"})
+        void singleInputTest(final String name) {
+            assertSimpleTest(() -> subject(name));
+            assertThat(getOutput()).contains(name);
         }
+
+        @DisplayName("두개의 입력을 받으면, 두개의 입력을 출력한다.")
+        @ParameterizedTest
+        @CsvSource({"fortune,29", "seed,30", "joy,31"})
+        void doubleInputTest(final String name, final String age) {
+            assertSimpleTest(() -> subject(name, age));
+            assertThat(getOutput()).contains(name, age);
+        }
+    }
+
+    private String getOutput() {
+        return captor.toString().trim();
     }
 }
