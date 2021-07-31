27092e307c862fb24e6d23cb9d7176bab225d1ff
김주원 <kjw11077@naver.com>
2021-08-01 02:24:20 +0900

    refactor: Randoms 파라미터 startInclusive,endInclusive -> start,end로 리네임

--- src/main/java/com/woowahan/techcourse/utils/Randoms.java
+++ src/main/java/com/woowahan/techcourse/utils/Randoms.java
@@ -1,9 +1,6 @@
 package com.woowahan.techcourse.utils;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 public class Randoms {
 
@@ -12,23 +9,23 @@ public class Randoms {
     private Randoms() {
     }
 
-    public static int pick(final int startInclusive, final int endInclusive) {
-        validateRange(startInclusive, endInclusive);
+    public static int pick(final int start, final int end) {
+        validateRange(start, end);
 
-        return startInclusive + RANDOM.nextInt(endInclusive - startInclusive + 1);
+        return start + RANDOM.nextInt(end - start + 1);
     }
 
-    private static void validateRange(final int startInclusive, final int endInclusive) {
-        if (startInclusive > endInclusive) {
-            throw new IllegalArgumentException("startInclusive가 endInclusive보다 클 수 없습니다.");
+    private static void validateRange(final int start, final int end) {
+        if (start > end) {
+            throw new IllegalArgumentException("start가 end보다 클 수 없습니다.");
         }
     }
 
-    public static List<Integer> notDuplicatedPicks(final int startInclusive, final int endInclusive, final int count) {
-        validateIntsRange(startInclusive, endInclusive, count);
+    public static List<Integer> picks(final int start, final int end, final int count) {
+        validateIntsRange(start, end, count);
         List<Integer> randomInts = new ArrayList<>();
 
-        for (int i = startInclusive; i <= endInclusive; ++i) {
+        for (int i = start; i <= end; ++i) {
             randomInts.add(i);
         }
 
@@ -36,13 +33,13 @@ public class Randoms {
         return randomInts.subList(0, count);
     }
 
-    private static void validateIntsRange(final int startInclusive, final int endInclusive, final int count) {
+    private static void validateIntsRange(final int start, final int end, final int count) {
         if (count < 0) {
             throw new IllegalArgumentException("count는 0보다 작을 수 없습니다.");
         }
 
-        if (endInclusive - startInclusive + 1 < count) {
-            throw new IllegalArgumentException("count가 (endInclusive - startInclusive + 1) 보다 클 수 없습니다.");
+        if (end - start + 1 < count) {
+            throw new IllegalArgumentException("count가 (end - start + 1) 보다 클 수 없습니다.");
         }
     }
 
