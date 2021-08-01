fd3696475cab1bfbd85f462dc01ab1db90231f15
김주원 <kjw11077@naver.com>
2021-08-01 12:55:30 +0900

    feat: picks 검증로직 추가

--- src/main/java/com/woowahan/techcourse/utils/Randoms.java
+++ src/main/java/com/woowahan/techcourse/utils/Randoms.java
@@ -34,12 +34,16 @@ public class Randoms {
     }
 
     private static void validateIntsRange(final int start, final int end, final int count) {
+        if (start > end) {
+            throw new IllegalArgumentException("start가 end보다 클 수 없습니다.");
+        }
+
         if (count < 0) {
             throw new IllegalArgumentException("count는 0보다 작을 수 없습니다.");
         }
 
         if (end - start + 1 < count) {
-            throw new IllegalArgumentException("count가 (end - start + 1) 보다 클 수 없습니다.");
+            throw new IllegalArgumentException("start에서 end의 범위보다 count가 작을 수 없습니다.");
         }
     }
 
